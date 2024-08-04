49722224c9e0b93442146679544ab58ba88fe362
dsthiago <thiagosd.2.18@gmail.com>
2024-08-04 08:37:51 -0300

    `rccn_101_conf1_v1` finished

--- src/bullet_from_a_gun/pipeline_registry.py
+++ src/bullet_from_a_gun/pipeline_registry.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline
 
-# from .pipelines.detectron2 import create_pipeline as detectron2_pipeline
+from .pipelines.detectron2 import create_pipeline as detectron2_pipeline
 # from .pipelines.mask_rcnn import create_pipeline as mask_rcnn_pipeline
 from .pipelines.yolo import create_pipeline as yolo_pipeline
 # from .pipelines.detr import create_pipeline as detr_pipeline
@@ -15,14 +15,14 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    # detectron2 = detectron2_pipeline()
+    detectron2 = detectron2_pipeline()
     # mask_rcnn = mask_rcnn_pipeline()
     yolo = yolo_pipeline()
     # detr = detr_pipeline()
     return {
         # "__default__" : detectron2 + mask_rcnn + yolo + detr,
-        "__default__" : yolo,
-        # "detectron2" : detectron2,
+        "__default__" : yolo + detectron2,
+        "detectron2" : detectron2,
         # "mask_rcnn"  : mask_rcnn,
         "yolo"       : yolo,
         # "detr"       : detr,
